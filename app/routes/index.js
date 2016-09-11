51762d5da4f74c183400172db6689b5d6f620528
Russvetsper <russvetsper@gmail.com>
2016-09-10 17:45:30 -0700

    add index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
+var questions = [{
+  id:1,
+  author: "mike"
+  post:"whre can i learn how to sail?"
+}, {
+
+  id:2,
+  author: "John"
+  post: "where can i find good pizza in seattle?"
+}];
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  }
 });
