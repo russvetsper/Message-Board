9541891e143114f62d81ef3a0341fe0b39b3274e
Russvetsper <russvetsper@gmail.com>
2016-09-10 17:59:16 -0700

    add firebase package

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,16 @@ import Ember from 'ember';
 
 var questions = [{
   id:1,
-  author: "mike"
-  post:"whre can i learn how to sail?"
+  author: "mike",
+  post:"whre can i learn how to sail?",
 }, {
 
   id:2,
-  author: "John"
-  post: "where can i find good pizza in seattle?"
+  author: "John",
+  post: "where can i find good pizza in seattle?",
 }];
 export default Ember.Route.extend({
   model() {
     return questions;
-  }
+  },
 });
